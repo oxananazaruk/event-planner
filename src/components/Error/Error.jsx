78674e85c37693a243390efcc140fb4ec1338e85
oxananazaruk <kurylooxana99@gmail.com>
2024-09-23 14:24:00 +0300

    updadet link in  read.me

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -3,7 +3,7 @@ import { Warning } from './Error.styled';
 export const Error = () => {
   return (
     <Warning>
-      Oops! Something went wrong! Please try reloading this page!
+      Oops! Something went wrong... Please try reloading this page!
     </Warning>
   );
 };
